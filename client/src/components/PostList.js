66fa6da38dd7737421e62857a777fb676deb3d3e
mohamed-elzohery <m.zohery1998@gmail.com>
2022-05-04 08:47:39 +0200

    Preparing react app for deploying on posts.com

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -7,7 +7,7 @@ const PostList = () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const {data: newPosts} = await axios.get('http://localhost:4002/posts');
+        const {data: newPosts} = await axios.get('http://posts.com/posts');
         setPosts(newPosts);
     }
     useEffect(() => {
